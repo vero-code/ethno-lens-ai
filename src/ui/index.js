541debd792583c4f979ab4a2130976abb827f3ac
vero-code <79207983+vero-code@users.noreply.github.com>
2025-07-06 20:59:02 +0300

    init: scaffold Express Add-on project

---
+++ src/ui/index.js
@@ -0,0 +1,30 @@
+import addOnUISdk from "https://new.express.adobe.com/static/add-on-sdk/sdk.js";
+
+addOnUISdk.ready.then(async () => {
+    console.log("addOnUISdk is ready for use.");
+
+    // Get the UI runtime.
+    const { runtime } = addOnUISdk.instance;
+
+    // Get the proxy object, which is required
+    // to call the APIs defined in the Document Sandbox runtime
+    // i.e., in the `code.js` file of this add-on.
+    const sandboxProxy = await runtime.apiProxy("documentSandbox");
+
+    const createRectangleButton = document.getElementById("createRectangle");
+    createRectangleButton.addEventListener("click", async event => {
+        await sandboxProxy.createRectangle();
+    });
+
+    const addTextButton = document.getElementById("addText");
+    addTextButton.addEventListener("click", async event => {
+        await sandboxProxy.addText();
+    });
+
+    // Enable the button only when:
+    // 1. `addOnUISdk` is ready,
+    // 2. `sandboxProxy` is available, and
+    // 3. `click` event listener is registered.
+    createRectangleButton.disabled = false;
+    addTextButton.disabled = false;
+});
