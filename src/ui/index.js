23c4e0bad2d5a8b1da74da57cf24456b15e4280f
vero-code <79207983+vero-code@users.noreply.github.com>
2025-10-23 08:46:10 +0300

    refactor: remove custom tabs logic after migration to sp-tabs

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,6 +1,5 @@
 // src/ui/index.js
 import addOnUISdk from "https://new.express.adobe.com/static/add-on-sdk/sdk.js";
-import { initializeTabs } from './tabs.js';
 import { initializeDesignPanel } from './panel/designPanel.js';
 import { initializeImagePanel } from './panel/imagePanel.js';
 
@@ -9,7 +8,6 @@ const IS_DEV_MODE = true;
 addOnUISdk.ready.then(async () => {
     console.log("addOnUISdk is ready for use.");
 
-    initializeTabs();
     const { runtime } = addOnUISdk.instance;
     const sandboxProxy = await runtime.apiProxy("documentSandbox");
 
